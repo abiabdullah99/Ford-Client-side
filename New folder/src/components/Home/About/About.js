00769d757f8af78d8f53ee821e4135ce0af3742d
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-22 07:45:05 +0600

    Responsive About Section

--- New folder/src/components/Home/About/About.js
+++ New folder/src/components/Home/About/About.js
@@ -3,17 +3,23 @@ import about from "../../../Assets/Banner/aboutus.png";
 const About = () => {
   return (
     <div>
-      <div class="hero">
-        <div class="hero-content flex-col lg:flex-row">
-          <img src={about} alt="" class="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 class="text-5xl font-bold">Box Office News!</h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+      <div class="hero justify-center lg:px-20 mt-40 px-10">
+        <div class="hero-content flex-col lg:flex-row lg:ml-20">
+          <img src={about} alt="" class="md:max-w-lg max-w-sm shadow-2xl" />
+          <div className="pl-10 mt-10">
+            <p className="text-secondary">Let Me Introduce</p>
+            <h1 class="text-5xl font-bold text-primary">Our Company</h1>
+            <p class="py-6 text-secondary text-justify">
+              For over 29 years, the Industify family has been building
+              relationships and projects that last. We build safe environments
+              and eco-friendly solutions in the communities in which we work.
+              Most importantly, we build strong relationships that allow us to
+              build anything, anywhere. No matter the job, we go beyond
+              building.
             </p>
-            <button class="btn btn-primary">Get Started</button>
+            <button class="btn btn-primary text-white text-lg capitalize rounded">
+              More Info
+            </button>
           </div>
         </div>
       </div>
